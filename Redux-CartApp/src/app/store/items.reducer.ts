08988fb7d3c423c964124dd22216a2a8742d8459
LuisEC10 <luisicpna10@gmail.com>
2025-09-01 01:46:52 -0500

    avance-redux-cartapp

---
+++ Redux-CartApp/src/app/store/items.reducer.ts
@@ -0,0 +1,54 @@
+import { createReducer, on } from "@ngrx/store";
+import { CartItem } from "../models/cartItem";
+import { add, remove, total } from "./items.action";
+
+export interface ItemsState {
+    items: CartItem[],
+    total: number
+};
+
+export const initialState: ItemsState = {
+    items: JSON.parse(sessionStorage.getItem('cart') || "[]"),
+    total: 0,
+};
+
+export const itemsReducer = createReducer(
+    initialState,
+    on(add, (state, { product }) => {
+        const hasItem = state.items.find((item: CartItem) => {
+            return item.product.id === product.id;
+        });
+        if (hasItem) {
+            return {
+                items: state.items = state.items.map((item: CartItem) => {
+                    if (item.product.id === product.id) {
+                        return {
+                            ...item,
+                            quantity: item.quantity + 1
+                        }
+                    }
+                    return item;
+                }),
+                total: state.total
+            }
+        } else {
+            return {
+                items: [...state.items, { product: { ...product }, quantity: 1 }],
+                total: state.total
+            };
+        }
+    }),
+    on(remove, (state, {id}) => {
+        console.log(id);
+        return {
+            items: state.items.filter( item => item.product.id !== id ),
+            total: state.total
+        }
+    }),
+    on(total, (state) => {
+        return {
+            items: state.items,
+            total: state.items.reduce((acumulator, item) => acumulator + item.quantity * item.product.price, 0),
+        }
+    })
+);
