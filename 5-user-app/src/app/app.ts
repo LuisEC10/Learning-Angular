3f35d8a57b1667891d18a41bc7667f3ecad1a65e
LuisEC10 <luisicpna10@gmail.com>
2025-10-09 10:42:30 -0500

    5-app-user

---
+++ 5-user-app/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserAppComponent } from "./components/user-app.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, UserAppComponent],
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('user-app');
+}
---
+++ 5-user-app/src/app/components/user.component/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'user',
+  imports: [],
+  templateUrl: './user.component.html',
+})
+export class UserComponent {
+  @Input() users: User[] = [];
+
+  @Output() idUserEventEmitter = new EventEmitter();
+
+  @Output() selectedUserEventEmitter = new EventEmitter();
+
+  onRemoveUser(id: number): void {
+    this.idUserEventEmitter.emit(id)
+  }
+
+  onUpdateUser(user: User): void {
+    this.selectedUserEventEmitter.emit(user);
+  }
+}
